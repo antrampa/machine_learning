370326ce8e3d66affa6392e01706e8403a057dcf
Antonis Zalonis <antonis.zalonis@gmail.com>
2023-07-27 23:35:13 +0300

    Plot iris data

---
+++ chapter2/plot_iris_dataset.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# df = pd.read_csv(
+#     "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data",
+#     header=None,
+# )
+df = pd.read_csv("local_storage/iris.data", header=None)
+df.tail()
+
+# select setosa and versicolor
+y = df.iloc[0:100, 4].values
+y = np.where(y == "Iris-setosa", -1, 1)
+
+# extract sepal length and petal length
+X = df.iloc[0:100, [0, 2]].values
+
+# plot data
+plt.scatter(X[:50, 0], X[:50, 1], color="red", marker="o", label="setosa")
+plt.scatter(X[50:100, 0], X[50:100, 1], color="blue", marker="x", label="versicolor")
+plt.xlabel("sepal length [cm]")
+plt.ylabel("petal length [cm]")
+plt.legend(loc="upper left")
+plt.show()
